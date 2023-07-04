453819c7c92acaef9e358b26ee021bfb2e315173
voskanvl <voskanvl@gmail.com>
2023-07-04 09:39:29 +0300

    add next-auth

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
 
 export async function GET(req: Request) {
-    return NextResponse.json({ a: "HELLO!" })
+    return NextResponse.json({ a: process.env.SECRET_DATA })
 }
